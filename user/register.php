8dec05a7e8c8ed15cfd4b53c132ac779d02a7225
francesyana <franceslorraine19@gmail.com>
2025-11-06 11:26:33 +0800

    updated register

--- user/register.php
+++ user/register.php
@@ -3,26 +3,28 @@ session_start();
 include("../includes/header.php");
 ?>
 
-<div class="container-fluid container-lg mt-4">
+<div class="container">
+  <div class="auth-container">
     <?php include("../includes/alert.php"); ?>
     <form action="store.php" method="POST">
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+      </div>
 
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" required>
-        </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+      </div>
 
-        <div class="mb-3">
-            <label for="confirmPass" class="form-label">Confirm Password</label>
-            <input type="password" class="form-control" id="confirmPass" name="confirmPass" required>
-        </div>
+      <div class="mb-3">
+        <label for="confirmPass" class="form-label">Confirm Password</label>
+        <input type="password" class="form-control" id="confirmPass" name="confirmPass" required>
+      </div>
 
-        <button type="submit" class="btn btn-primary">Register</button>
+      <button type="submit" class="btn btn-primary w-100">Register</button>
     </form>
+  </div>
 </div>
 
 <?php include("../includes/footer.php"); ?>
