2caeb9bf15772637e16381c22853cd6773dd8e8e
fritzabcdefg <zizimimocha@gmail.com>
2025-11-06 11:33:54 +0800

    new login no header

--- user/login.php
+++ user/login.php
@@ -1,8 +1,20 @@
 <?php
 session_start();
-include("../includes/header.php");
 include("../includes/config.php");
+?>
 
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login - F&L Glam Co</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../includes/style/style.css">
+</head>
+<body>
+  
+<?php
 if (isset($_POST['submit'])) {
   $email = trim($_POST['email']);
   $pass = trim($_POST['password']);
@@ -21,6 +33,7 @@ if (isset($_POST['submit'])) {
       $_SESSION['user_id'] = $user_id;
       $_SESSION['role'] = $role;
       header("Location: ../user/profile.php");
+      exit();
     } else {
       $_SESSION['message'] = 'Wrong email or password';
     }
